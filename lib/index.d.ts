7e92b8433663c9b8c7afde594aed233c2fc0f812
Mac-A-4 <malcolm.alvarez9898@gmail.com>
2023-04-18 13:36:07 -0700

    `Build`

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+export * from './model/CreateAccount';
+export * from './model/CreateSession';
+export * from './model/OperationError';
+export * from './client/Operation';
+export * from './client/Client';
